911d252e2f9c6f886c69f077a2df826ecc1f61a2
Łukasz <wolskiaxiom@interia.pl>
2019-05-12 20:57:12 +0200

    add form for adding/editing firefighter

---
+++ frontend/src/components/forms/FirefighterForm.vue
@@ -0,0 +1,112 @@
+<template>
+    <div>
+        <b-button v-b-modal="this.form.id">Dodaj strażaka</b-button>
+
+        <b-modal
+            :id="this.form.id"
+            ref="modal"
+            title="Podaj dane strażaka"
+            @show="readFirefighterData"
+            @hidden="readFirefighterData"
+            hide-footer
+            >
+            <b-form ref="form" @submit="onSubmit" @reset="onReset">
+
+                <b-form-group id="input-group-surname" label="Nazwisko: " label-for="name_id">
+                    <b-form-input
+                            id="surname_id"
+                            v-model="form.surname"
+                            required
+                            placeholder="Wpisz nazwisko"
+                    ></b-form-input>
+                </b-form-group>
+
+                <b-form-group id="input-group-name" label="Imię: " label-for="name_id">
+                    <b-form-input
+                            id="name_id"
+                            v-model="form.name"
+                            required
+                            placeholder="Wpisz imię"
+                    ></b-form-input>
+                </b-form-group>
+
+                <b-form-group id="input-group-age" label="Wiek: " label-for="age_id">
+                    <b-form-input
+                            id="age_id"
+                            v-model="form.age"
+                            required
+                            placeholder="Wpisz wiek"
+                    ></b-form-input>
+                </b-form-group>
+
+                <b-button type="submit" variant="primary">Dodaj</b-button>
+                <b-button type="reset" variant="danger">Wyczyść</b-button>
+            </b-form>
+
+
+        </b-modal>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        props:{
+            firefighterData:{
+                type: Object,
+                default(){
+                    return {
+                        id: 'defaultId',
+                        surname: '',
+                        name: '',
+                        age:''
+                    }
+                }
+
+            }
+        },
+        data() {
+            return {
+                form: {
+                    id: '',
+                    surname: '',
+                    name: '',
+                    age: ''
+                },
+            }
+        },
+        created() {
+            this.form.id = this.firefighterData.id;
+            this.form.surname = this.firefighterData.surname;
+            this.form.name = this.firefighterData.name;
+            this.form.age = this.firefighterData.age;
+        },
+        methods: {
+            onSubmit(evt) {
+                evt.preventDefault();
+                alert(JSON.stringify(this.form));
+            },
+            onReset(evt) {
+                evt.preventDefault()
+                this.readFirefighterData()
+            },
+            readFirefighterData(){
+                    this.form.name = this.firefighterData.name;
+                    this.form.id = this.firefighterData.id;
+                    this.form.surname = this.firefighterData.surname;
+                    this.form.age = this.firefighterData.age;
+            }
+            // ,
+            // resetModal(){
+            //     // this.form.name='';
+            //     // this.form.age='';
+            //     // this.form.surname='';
+            //     this.readFirefighterData();
+            // }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
